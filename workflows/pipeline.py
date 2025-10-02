8b8f0f914eb7c65987ee26a07a6ac70a8debe73d
nuuuwan <nuuuwan@gmail.com>
2025-10-02 10:45:08 +0530

    Multi-doc

--- workflows/pipeline.py
+++ workflows/pipeline.py
@@ -1,4 +1,11 @@
-from lk_tourism import MonthlyReports
+import sys
+
+from lk_tourism import MonthlyReports, WeeklyReports
 
 if __name__ == "__main__":
-    MonthlyReports.run_pipeline()
+    doc_class_label = sys.argv[1]
+    for doc_class in [MonthlyReports, WeeklyReports]:
+        if doc_class.get_doc_class_label() == doc_class_label:
+            doc_class.run_pipeline()
+            sys.exit(0)
+    raise ValueError(f"Unknown doc_class_label: {doc_class_label}")
