68832fd2c34d189e7da39c8912b6c764ffe00fa0
nuuuwan <nuuuwan@gmail.com>
2025-10-02 10:51:29 +0530

    log.debug(f"{doc_class_label=}")

--- workflows/pipeline.py
+++ workflows/pipeline.py
@@ -1,9 +1,14 @@
 import sys
 
+from utils import Log
+
 from lk_tourism import MonthlyReports, WeeklyReports
+
+log = Log("pipeline")
 
 if __name__ == "__main__":
     doc_class_label = sys.argv[1]
+    log.debug(f"{doc_class_label=}")
     for doc_class in [MonthlyReports, WeeklyReports]:
         if doc_class.get_doc_class_label() == doc_class_label:
             doc_class.run_pipeline()
